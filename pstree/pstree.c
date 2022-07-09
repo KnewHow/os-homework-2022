5d7eb3f3fde8d5fd13036ac8e8501410cb664894
knewhow <how.yuangh@gmail.com>
2022-07-09 20:40:44 +0800

    sucess retrieve process information

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -39,6 +39,38 @@ struct Process
   int ppid;
 };
 
+void constructProcess(const char* stat, struct Process *processList) {
+  char temp[128];
+  int index = 0;
+  memset(temp, 0, sizeof(temp));
+  const char *p = stat;
+  while (*p != '\0')
+  {
+    if(*p != ' ') {
+      temp[index++] = *p;
+      ++p;
+    } else {
+      break;
+    }
+  }
+  
+  int pid = atoi(temp);
+  index = 0;
+  memset(temp, 0, sizeof(temp));
+  p += 2;
+  while(*p != ')') {
+    temp[index++] = *p;
+    ++p;
+  }
+  char state;
+  int ppid;
+  sscanf(p+2, "%c %d", &state, &ppid);
+  processList[pid].pid = pid;
+  memcpy(processList[pid].comm, temp, strlen(temp));
+  processList[pid].state = state;
+  processList[pid].ppid = ppid;
+}
+
 
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
@@ -62,26 +94,26 @@ int main(int argc, char *argv[]) {
   struct dirent *dir;
   if(d) {
     while((dir = readdir(d)) != NULL) {
-     if(isNumber(dir->d_name)) {
-      int dname = atoi(dir->d_name);
-      char filepath[1000];
-      sprintf(filepath, "/proc/%d/stat", dname);
-      FILE *f = fopen(filepath, "r");
-      int pid;
-      char comm[1024];
-      char state;
-      int ppid;
-      fscanf(f, "%d %s %c %d", &pid, comm, &state, &ppid);
-      printf("%d %s %c %d\n", pid, comm, state, ppid);
-      processList[dname].pid = pid;
-      strncpy(processList[dname].comm, comm, sizeof(comm));
-      processList[dname].state = state;
-      processList[dname].ppid = ppid;
-     }
+      if(isNumber(dir->d_name)) {
+        int dname = atoi(dir->d_name);
+        char filepath[1000];
+        sprintf(filepath, "/proc/%d/stat", dname);
+        FILE *f = fopen(filepath, "r");
+        char *line = NULL;
+        size_t length = 0;
+        if(getline(&line, &length, f) != -1) {
+          constructProcess(line, processList);
+        }
+      }
     }
     closedir(d);
   } else {
     printf("can't open path: %s\n", path);
+  }
+  for(int i = 0; i < pid_max; ++i) {
+    if(processList[i].pid != -1) {
+      printf("%d %s %c %d\n", processList[i].pid, processList[i].comm, processList[i].state, processList[i].ppid);
+    }
   }
   free(processList);
   assert(!argv[argc]);
